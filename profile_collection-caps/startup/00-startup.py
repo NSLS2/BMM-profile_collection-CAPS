d5fad250dbce560ba07bf90c799a8404dd0543ac
Thomas A Caswell <tcaswell@bnl.gov>
2024-12-12 00:00:43 -0500

    ENH: add tasks and more profile

--- profile_collection-caps/startup/00-startup.py
+++ profile_collection-caps/startup/00-startup.py
@@ -1,5 +1,14 @@
+from bluesky import RunEngine
 from bluesky.callbacks.tiled_writer import TiledWriter
 from tiled.client import from_uri
+from bluesky.callbacks.best_effort import BestEffortCallback
 
+import bluesky.plans as bp
+
+bec = BestEffortCallback()
 
 cat = from_uri('http://127.0.0.1:8000', api_key='capscaps')
+tw = TiledWriter(cat)
+RE = RunEngine({})
+RE.subscribe(tw)
+RE.subscribe(bec)
